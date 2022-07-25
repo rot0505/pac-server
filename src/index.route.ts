1ebc6b892e735629887a87eb02a521252b9e8eb0
MantaDreamer <mantadreamer@gmail.com>
2022-07-25 23:39:35 +0900

    updateItemManage

--- src/index.route.ts
+++ src/index.route.ts
@@ -4,6 +4,7 @@ const router = express.Router()
 const authRoutes = require('./routes/auth');
 const furnitureRoutes = require('./routes/furniture');
 const itemRoutes = require('./routes/item');
+const equipmentRoutes = require('./routes/equipment');
 
 router.get('/health-check', (req, res) =>
   res.send('OK')
@@ -12,5 +13,6 @@ router.get('/health-check', (req, res) =>
 router.use('/auth', authRoutes);
 router.use('/furniture', furnitureRoutes);
 router.use('/item', itemRoutes);
+router.use('/equipment', equipmentRoutes);
 
 module.exports = router;
